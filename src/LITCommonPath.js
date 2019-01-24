d06d8c7d84a9148edbbc4a3798ac8487582de058
Tao Yunfei <traintrackcn@gmail.com>
2019-01-24 11:13:33 +0800

    *...

--- src/LITCommonPath.js
+++ src/LITCommonPath.js
@@ -3,9 +3,9 @@ import LITPath from './LITPath';
 export default class LITCommonPath extends LITPath{
     constructor(path){
         super(path);
+        // console.log('path -> '+this.path+' '+this.constructor.name);
         this.value = path.concat(['value']);
         this.index = path.concat(['index']);
         this.collection = path.concat(['collection']);
     }
 }
- 
