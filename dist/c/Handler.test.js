ba45b73be67175defb93ba791aea6ca3766eb1ac
Tao Yunfei <traintrackcn@gmail.com>
2019-08-03 20:13:51 +0800

    * added tests for c.Handler

---
+++ dist/c/Handler.test.js
@@ -0,0 +1,81 @@
+"use strict";
+
+var _Handler = _interopRequireDefault(require("./Handler"));
+
+var _Key = _interopRequireDefault(require("./Key"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+describe('', function () {
+  var handler = new _Handler["default"]();
+  beforeAll(function () {
+    handler.parse(raw);
+  });
+  it('current map', function () {
+    expect(handler.map).toMatchSnapshot();
+  });
+  it('get raw node "config,menu"', function () {
+    var key = new _Key["default"]();
+    key.value = 'config,menu';
+    expect(handler.get_raw_node(key)).toMatchSnapshot();
+  });
+  it('get node "config,menu"', function () {
+    var key = new _Key["default"]();
+    key.value = 'config,menu';
+    expect(handler.get_node(key)).toMatchSnapshot();
+  });
+  it('get node "config,menu,web" base rule', function () {
+    var key = new _Key["default"]();
+    key.value = 'config,menu,WEB';
+    expect(handler.get(key)).toMatchSnapshot();
+    var ctx = ['prj1'];
+    expect(handler.get(key, ctx)).toMatchSnapshot();
+  });
+});
+var raw = [{
+  "title": "menu",
+  "expanded": true,
+  "children": [{
+    "title": "SERVICE",
+    "active": true
+  }, {
+    "title": "BOT",
+    "active": true
+  }, {
+    "title": "CONFIG",
+    "expanded": true,
+    "active": true
+  }]
+}, {
+  "title": "config",
+  "expanded": true,
+  "children": [{
+    "title": "menu",
+    "expanded": false,
+    "children": [{
+      "title": "WEB",
+      "r": [{
+        "on": true,
+        "base": "LIT"
+      }, {
+        "_": ["prj1"],
+        "base": "OTHER"
+      }],
+      "active": true
+    }, {
+      "title": "APP",
+      "r": [{
+        "on": true,
+        "base": "LIT"
+      }],
+      "expanded": true,
+      "active": true
+    }, {
+      "title": "API",
+      "active": true
+    }, {
+      "title": "MISC",
+      "active": true
+    }]
+  }]
+}];
---
+++ src/c/Handler.test.js
@@ -0,0 +1,107 @@
+import Handler from "./Handler";
+import Key from "./Key";
+
+describe('', () => {
+    const handler = new Handler();
+    beforeAll(()=>{
+        handler.parse(raw);
+    });
+
+    it('current map', () => {
+        expect(handler.map).toMatchSnapshot();
+    });
+
+    it('get raw node "config,menu"', () => {
+        const key = new Key();
+        key.value = 'config,menu';
+        expect(handler.get_raw_node(key)).toMatchSnapshot();
+    });
+
+    it('get node "config,menu"', () => {
+        const key = new Key();
+        key.value = 'config,menu';
+        expect(handler.get_node(key)).toMatchSnapshot();
+    });
+
+    it('get node "config,menu,web" base rule', () => {
+        const key = new Key();
+        key.value = 'config,menu,WEB';
+        expect(handler.get(key)).toMatchSnapshot();
+
+
+        const ctx = ['prj1'];
+        expect(handler.get(key, ctx)).toMatchSnapshot();
+    });
+
+});
+
+
+
+const raw = [
+    {
+      "title": "menu",
+      "expanded": true,
+      "children": [
+        {
+          "title": "SERVICE",
+          "active": true
+        },
+        {
+          "title": "BOT",
+          "active": true
+        },
+        {
+          "title": "CONFIG",
+          "expanded": true,
+          "active": true
+        }
+      ]
+    },
+    {
+      "title": "config",
+      "expanded": true,
+      "children": [
+        {
+          "title": "menu",
+          "expanded": false,
+          "children": [
+            {
+              "title": "WEB",
+              "r": [
+                {
+                  "on": true,
+                  "base": "LIT"
+                },
+                {
+                  "_": [
+                    "prj1"
+                  ],
+                  "base": "OTHER"
+                }
+              ],
+              "active": true
+            },
+            {
+              "title": "APP",
+              "r": [
+                {
+                  "on": true,
+                  "base": "LIT"
+                }
+              ],
+              "expanded": true,
+              "active": true
+            },
+            {
+              "title": "API",
+              "active": true
+            },
+            {
+              "title": "MISC",
+              "active": true
+            }
+          ]
+        }
+      ]
+    }
+];
